d9b09304dd337197fe27d45896587033f19c4382
heromiya <heromiya@heromiya.net>
2014-03-08 09:36:18 +0800

    Revision of achievements.php

--- achievements.php
+++ achievements.php
@@ -75,10 +75,10 @@ $sql = sprintf("select username
 	$latmin=$row['latmin'];
 	$lonmax=$row['lonmax'];
 	$latmax=$row['latmax'];
-	$refimage=$row['refimage_gid'];
+	$refimage_gid=$row['refimage_gid'];
 	$pixelsize=0.0001388888888888888888888;
 	$thumbsize=96;
-	$wmsthumb='http://'.$WEBHOST.'/cgi-bin/mapserv?map=/var/www/dawei/landsat.map&SERVICE=WMS&REQUEST=GetMap&VERSION=1.1.1&LAYERS='.$refimage.'&WIDTH='.$thumbsize.'&HEIGHT='.$thumbsize.'&SRS=EPSG:4326&FORMAT=JPEG&BBOX=';
+	$wmsthumb='http://'.$WEBHOST.'/cgi-bin/mapserv?map=/var/www/dawei/landsat.map&SERVICE=WMS&REQUEST=GetMap&VERSION=1.1.1&LAYERS='.$refimage_gid.'&WIDTH='.$thumbsize.'&HEIGHT='.$thumbsize.'&SRS=EPSG:4326&FORMAT=JPEG&BBOX=';
 	$tiles='<a href="javascript:void(0);" onClick="writemapthumb('.$thumbsize.','.$lonmin.','.$latmin.','.$lonmax.','.$latmax.','.$i.','."'thumb'".',"'.$refimage_gid.'");" id="thumb'.$i.'">Thumbnail</a>';
 
 	if(preg_match('/.*'.$username.'.*/',$row["approver"])){
@@ -101,9 +101,8 @@ $sql = sprintf("select username
     }
 $html_finished = $html_finished . "</table>";
 
-
 $sql = sprintf("select username
-                      ,assignment_gmaptile_13.gid
+                      ,dawei_assignment.gid
                       ,end_ts
                       ,ST_XMax(ST_Transform(the_geom,4326)) as lonmax
                       ,ST_XMin(ST_Transform(the_geom,4326)) as lonmin
@@ -111,15 +110,15 @@ $sql = sprintf("select username
                       ,ST_YMin(ST_Transform(the_geom,4326)) as latmin
 		      ,ARRAY_TO_STRING(
 			ARRAY(
-			    SELECT approval.username FROM approval
-			    WHERE approval.qid=assignment_gmaptile_13.gid
-			    ORDER BY approval.username
+			    SELECT dawei_approval.username FROM dawei_approval
+			    WHERE dawei_approval.qid=dawei_assignment.gid
+			    ORDER BY dawei_approval.username
 			    ), '<br>') AS approver
-                from assignment_gmaptile_13
-                left join target_tiles_gmaptile_13 on assignment_gmaptile_13.gid = target_tiles_gmaptile_13.gid 
+                from dawei_assignment
+                left join dawei_target on dawei_assignment.gid = dawei_target.gid 
                 where end_ts is not null 
                   and end_ts > '2100-01-01'
-				  and assignment_gmaptile_13.username = '" . $username . "'
+				  and dawei_assignment.username = '" . $username . "'
                 order by end_ts desc;");
   if (!($rs = pg_exec($sql))) {
     die;
@@ -141,11 +140,12 @@ $sql = sprintf("select username
 	$latmin=$row['latmin'];
 	$lonmax=$row['lonmax'];
 	$latmax=$row['latmax'];
+	$refimage_gid=$row['refimage_gid'];
 	$pixelsize=0.0001388888888888888888888;
 	$thumbsize=96;
 	$wmsthumb='http://'.$WEBHOST.'/cgi-bin/mapserv?map=/var/www/dawei/landsat.map&SERVICE=WMS&REQUEST=GetMap&VERSION=1.1.1&LAYERS='.$refimage.'&WIDTH='.$thumbsize.'&HEIGHT='.$thumbsize.'&SRS=EPSG:4326&FORMAT=JPEG&BBOX=';
 	//	$tiles='<img height="'.$thumbsize.'" width="'.$thumbsize.'" src="'.$wmsthumb.$lonmin.','.$latmin.','.$lonmax.','.$latmax.'">';
-	$tiles='<a href="javascript:void(0);" onClick="writemapthumb('.$thumbsize.','.$lonmin.','.$latmin.','.$lonmax.','.$latmax.','.$i.','."'thumb_unfinished'".');" id="thumb_unfinished'.$i.'">Thumbnail</a>';
+	$tiles='<a href="javascript:void(0);" onClick="writemapthumb('.$thumbsize.','.$lonmin.','.$latmin.','.$lonmax.','.$latmax.','.$i.','."'thumb_unfinished'".',"'.$refimage_gid.'");" id="thumb_unfinished'.$i.'">Thumbnail</a>';
 
 	if(preg_match('/.*'.$username.'.*/',$row["approver"])){
 		$approvestr = "<td>You have approved this.<br><form method='post' action='".$_SERVER['SCRIPT_NAME']."'><input type='hidden' name='unapprove' value='1'><input type='hidden' name='gid' value='".$row["gid"]."'><input name='submit_unapproval' type='submit' value='Cancel approval'></form></td>";
@@ -156,7 +156,7 @@ $sql = sprintf("select username
 	$html_unfinished = $html_unfinished . "<tr align='center'><td>" . $row["username"] . "</td>"
 	    . "<td>" . $row["gid"] . "</td>"
 	    . "<td>" . $tiles . "</td>"
-	    . "<td><a href='wms.tms.parallel.v2.php?lonmin=". $row["lonmin"] ."&latmin=". $row["latmin"] ."&lonmax=". $row["lonmax"] ."&latmax=". $row["latmax"] ."'>Revise</a>" . "</td>"
+	    . "<td><a href='wms.tms.parallel.v2.php?lonmin=". $row["lonmin"] ."&latmin=". $row["latmin"] ."&lonmax=". $row["lonmax"] ."&latmax=". $row["latmax"] ."&refimage_gid=". $row["refimage_gid"] .'>Revise</a>" . "</td>"
 	    . "<td><form method='post' action='".$_SERVER['SCRIPT_NAME']."'><input type='hidden' name='finished' value='1'><input type='hidden' name='gid' value='".$row["gid"]."'><input name='submit_unfinished' type='submit' value='Mark as finished'></form>
 		</td>"
 	    . $approvestr
@@ -209,7 +209,7 @@ th {
 // -->
 </style>
 <body>
-<a href="http://guam.csis.u-tokyo.ac.jp/verify/assignregion.php">Back to assignment</a>
+<a href="assignregion.php">Back to assignment</a>
 <h3>Finished regions</h3>
 <?php echo $html_finished ?>
 <h3>Regions marked as unfinished</h3>
