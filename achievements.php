cc2d62470547bf7bcc8f5a91fd208a709664d6de
Hiroyuki Miyazaki <heromiya@gumaster>
2014-03-08 11:10:05 +0900

    modified:   achievements.php

--- achievements.php
+++ achievements.php
@@ -79,7 +79,7 @@ $sql = sprintf("select username
 	$pixelsize=0.0001388888888888888888888;
 	$thumbsize=96;
 	$wmsthumb='http://'.$WEBHOST.'/cgi-bin/mapserv?map=/var/www/dawei/landsat.map&SERVICE=WMS&REQUEST=GetMap&VERSION=1.1.1&LAYERS='.$refimage_gid.'&WIDTH='.$thumbsize.'&HEIGHT='.$thumbsize.'&SRS=EPSG:4326&FORMAT=JPEG&BBOX=';
-	$tiles='<a href="javascript:void(0);" onClick="writemapthumb('.$thumbsize.','.$lonmin.','.$latmin.','.$lonmax.','.$latmax.','.$i.','."'thumb'".',"'.$refimage_gid.'");" id="thumb'.$i.'">Thumbnail</a>';
+	$tiles='<a href="javascript:void(0);" onClick="writemapthumb('.$thumbsize.','.$lonmin.','.$latmin.','.$lonmax.','.$latmax.','.$i.','."'thumb'".",'".$refimage_gid.'\');" id="thumb'.$i.'">Thumbnail</a>';
 
 	if(preg_match('/.*'.$username.'.*/',$row["approver"])){
 		$approvestr = "<td>You have approved this.<br><form method='post' action='".$_SERVER['SCRIPT_NAME']."'><input type='hidden' name='unapprove' value='1'><input type='hidden' name='gid' value='".$row["gid"]."'><input name='submit_unapproval' type='submit' value='Cancel approval'></form></td>";
@@ -103,6 +103,7 @@ $html_finished = $html_finished . "</table>";
 
 $sql = sprintf("select username
                       ,dawei_assignment.gid
+                      ,dawei_assignment.refimage_gid
                       ,end_ts
                       ,ST_XMax(ST_Transform(the_geom,4326)) as lonmax
                       ,ST_XMin(ST_Transform(the_geom,4326)) as lonmin
@@ -115,7 +116,7 @@ $sql = sprintf("select username
 			    ORDER BY dawei_approval.username
 			    ), '<br>') AS approver
                 from dawei_assignment
-                left join dawei_target on dawei_assignment.gid = dawei_target.gid 
+                left join dawei_target on dawei_assignment.gid = dawei_target.tilex || '-' ||  dawei_target.tiley || '-' ||  dawei_target.refimage_gid
                 where end_ts is not null 
                   and end_ts > '2100-01-01'
 				  and dawei_assignment.username = '" . $username . "'
@@ -145,7 +146,7 @@ $sql = sprintf("select username
 	$thumbsize=96;
 	$wmsthumb='http://'.$WEBHOST.'/cgi-bin/mapserv?map=/var/www/dawei/landsat.map&SERVICE=WMS&REQUEST=GetMap&VERSION=1.1.1&LAYERS='.$refimage.'&WIDTH='.$thumbsize.'&HEIGHT='.$thumbsize.'&SRS=EPSG:4326&FORMAT=JPEG&BBOX=';
 	//	$tiles='<img height="'.$thumbsize.'" width="'.$thumbsize.'" src="'.$wmsthumb.$lonmin.','.$latmin.','.$lonmax.','.$latmax.'">';
-	$tiles='<a href="javascript:void(0);" onClick="writemapthumb('.$thumbsize.','.$lonmin.','.$latmin.','.$lonmax.','.$latmax.','.$i.','."'thumb_unfinished'".',"'.$refimage_gid.'");" id="thumb_unfinished'.$i.'">Thumbnail</a>';
+	$tiles='<a href="javascript:void(0);" onClick="writemapthumb('.$thumbsize.','.$lonmin.','.$latmin.','.$lonmax.','.$latmax.','.$i.','."'thumb_unfinished'".",'".$refimage_gid.'\');" id="thumb_unfinished'.$i.'">Thumbnail</a>';
 
 	if(preg_match('/.*'.$username.'.*/',$row["approver"])){
 		$approvestr = "<td>You have approved this.<br><form method='post' action='".$_SERVER['SCRIPT_NAME']."'><input type='hidden' name='unapprove' value='1'><input type='hidden' name='gid' value='".$row["gid"]."'><input name='submit_unapproval' type='submit' value='Cancel approval'></form></td>";
@@ -156,7 +157,7 @@ $sql = sprintf("select username
 	$html_unfinished = $html_unfinished . "<tr align='center'><td>" . $row["username"] . "</td>"
 	    . "<td>" . $row["gid"] . "</td>"
 	    . "<td>" . $tiles . "</td>"
-	    . "<td><a href='wms.tms.parallel.v2.php?lonmin=". $row["lonmin"] ."&latmin=". $row["latmin"] ."&lonmax=". $row["lonmax"] ."&latmax=". $row["latmax"] ."&refimage_gid=". $row["refimage_gid"] .'>Revise</a>" . "</td>"
+	    . "<td><a href='wms.tms.parallel.v2.php?lonmin=". $row["lonmin"] ."&latmin=". $row["latmin"] ."&lonmax=". $row["lonmax"] ."&latmax=". $row["latmax"] ."&refimage_gid=".$refimage_gid."'>Revise</a>" . "</td>"
 	    . "<td><form method='post' action='".$_SERVER['SCRIPT_NAME']."'><input type='hidden' name='finished' value='1'><input type='hidden' name='gid' value='".$row["gid"]."'><input name='submit_unfinished' type='submit' value='Mark as finished'></form>
 		</td>"
 	    . $approvestr
@@ -171,28 +172,18 @@ $html_unfinished = $html_unfinished . "</table>";
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Achievements</title></head>
+    <title>Achievements</title></head>
     <script type="text/javascript"> 
     <!-- 
-
-    function check_reassign(){
-    	    if(window.confirm('Really reassign?')){
-    	    	    return true;
-    	    }
-    	    else{
-    	    	    window.alert('Cancelled');
-    	    	    return false;
-    	    }
-	}
+    
 function writemapthumb(thumbsize,lonmin,latmin,lonmax,latmax,rn,id,refimage_gid){
     wmsthumb='http://guam.csis.u-tokyo.ac.jp/cgi-bin/mapserv?map=/var/www/dawei/landsat.map&SERVICE=WMS&REQUEST=GetMap&VERSION=1.1.1&LAYERS='+refimage_gid+'&WIDTH='+thumbsize+'&HEIGHT='+thumbsize+'&SRS=EPSG:4326&FORMAT=JPEG&BBOX=';
     img='<img height="'+thumbsize+'" width="'+thumbsize+'" src="'+wmsthumb+lonmin+','+latmin+','+lonmax+','+latmax+'">';
-    //    document.thumb.src=wmsthumb+lonmin+','+latmin+','+lonmax+','+latmax;
     document.getElementById(id+rn).innerHTML = img;
 }
 
-    // -->
-    </script>
+// -->
+</script>
 
 <style type="text/css">
     <!--
